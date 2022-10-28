23099598a7e6b8bfbd8d4490fcb57fcafcbcb1be
kaushaltheeG <kaushalsk09@gmail.com>
2022-10-28 14:18:19 -0700

    Finished Anagrams

--- anagrams.rb
+++ anagrams.rb
@@ -7,12 +7,40 @@ end
 #p first_anagram?("rac", "car")
 
 def second_anagram?(str1, str2)
-    str1.chars.each do |char|
-        if str2.include?(char)
-            str2.delete!(char)
+    str1.chars.each do |char| #O(n)
+        if str2.include?(char) #O(n)
+            idx = str2.index(char) #O(n)
+            str2.slice!(idx) #O(n)
         end
     end
     str2.empty?
 end
 
-p second_anagram?("car", "rac")
+#eq = n * 3n => 3n^2 => O(n^2)
+#p second_anagram?("car", "rac")
+
+def third_anagram?(str1,str2)
+    sort_1 = str1.split('').sort! #log n to spliting into invidual arrays then n to recombine it => O(n log n)
+    sort_2 = str2.split('').sort! 
+    sort_1 == sort_2
+end 
+
+#O(n log n)
+    #quicksort worse case can be O(n^2) if sorted in descending order 
+#p third_anagram?("elvis", "lives")
+
+def fourth_anagram?(str1,str2)
+    hash = Hash.new(0)
+    str1.chars.each do |char| #O(n)
+        hash[char] += 1
+    end 
+
+    str2.chars.each do |char| #O(m)
+        hash[char] -= 1
+    end 
+
+    hash.values.all? {|val| val.zero?} #O(2n)
+end 
+
+# n + m + 2n => O(n + m) || O(n)
+p fourth_anagram?("elvis", "lives")
